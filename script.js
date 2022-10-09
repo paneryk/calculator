79be65009432bbead506046eb6a7577181ad79a2
paneryk <eryk.wojdyla@gmail.com>
2022-10-09 02:28:34 +0200

    All the JS code rewritten for better funcionality and clariness

--- script.js
+++ script.js
@@ -1,177 +1,127 @@
 const buttons = document.querySelector('#buttons');
 const screen = document.querySelector('#screen');
-let numberOne = null;
-let numberTwo = null;
-let operator = null;
-let prevAction = null;
-window.addEventListener('keydown', (evt) => {
-    console.log(evt.code);
-    switch (evt.code) {
-        case "Digit1":
-            numericalSelected(1);
-            break;
-        case "Digit2":
-            numericalSelected(2);
-            break;
-        case "Digit3":
-            numericalSelected(3);
-            break;
-        case "Digit4":
-            numericalSelected(4);
-            break;
-        case "Digit5":
-            numericalSelected(5);  
-            break;
-        case "Digit6":
-            numericalSelected(6);    
-            break; 
-        case "Digit7":
-            numericalSelected(7);    
-            break;
-        case "Digit8":
-            numericalSelected(8);
-            break;
-        case "Digit9":
-            numericalSelected(9);
-            break;
-        case "Digit0":
-            numericalSelected(0);
-            break;
-        case "Minus":
-            checkFunction('substract');
-            break;
-        case "Slash":
-            checkFunction('divide');
-            break;
-        case "KeyX":
-            checkFunction('divide');
-            break;
-        case "KeyC":
-            checkFunction('clear');
-            break;
-        case "Equal":
-            checkFunction('add');
-            break;
-        case "Enter":
-            checkFunction('equals');
-            break;
+const cal = {
+    operandOne: null,
+    operandTwo: null,
+    operation: null,
+    operandTwoPending: true,
+    tempoperand: null,
+    displayToBeErased: false,
+}
+
+buttons.addEventListener('click', evt => {
+    const clickedButton = evt.target;
+    if (clickedButton.id != 'buttons') {
+    clickedButton.classList.contains('functional') ? functionCall(clickedButton) : numberCall(clickedButton.innerText);
     }
 });
 
-buttons.addEventListener('click', (e) => { //listening to buttons ('click')
-    const targetButton = e.target;
-    if (targetButton.getAttribute('id') != 'buttons') listening(targetButton);
-});
-
-const listening = function(targetButton) {
-    if (targetButton.classList.contains('functional')) {
-        console.log(targetButton);
-        checkFunction(targetButton.getAttribute('id'));
+const numberCall = function(number) {
+    if (screen.innerText.length < 9) {
+    if (screen.innerText === '0') {
+        screen.innerText = number;
+    }
+    else if (cal.displayToBeErased === true) {
+        screen.innerText = number;
+        cal.displayToBeErased = false;
+        cal.operandTwoPending = false;
     }
     else {
-        numericalSelected(targetButton.getAttribute('id'));
+        screen.innerText += number;
+        if (!!cal.operandOne) cal.operandTwoPending = false;
     }
-}
- 
-const checkFunction = function(buttonAction) {
-    switch (buttonAction) {
-        case 'clear': 
-            numberOne = null;
-            numberTwo = null;
-            operator = null;
-            screen.textContent = '0';
-            console.log('clear');
-            break;
-        case 'add':
-            if (!!numberOne && !!numberTwo && !!operator) executeCalculation();
-            operator = 'add';
-            numberOne = screen.textContent;
-            console.log(numberOne+operator);
-            prevAction = 'add';
-            break;
-        case 'divide':
-            if (!!numberOne && !!numberTwo && !!operator) executeCalculation();
-            operator = 'divide';
-            numberOne = screen.textContent;
-            console.log(numberOne+operator);
-            prevAction = 'divide';
-            break;
-        case 'multiply':
-            if (!!numberOne && !!numberTwo && !!operator) executeCalculation();
-            operator = 'multiply';
-            numberOne = screen.textContent;
-            console.log(numberOne+operator);
-            prevAction = 'multiply';
-            break;
-        case 'substract':
-            if (!!numberOne && !!numberTwo && !!operator) executeCalculation();
-            numberOne = screen.textContent;
-            console.log(numberOne+operator);
-            prevAction = 'substract';
-            operator = 'substract';
-            break;
-        case 'equals':
-            prevAction = operator;
-            if (!!numberOne && !!numberTwo && !!operator) { executeCalculation(); } 
-            break;
-        case 'comma':
-            !screen.textContent.includes('.') ? screen.textContent += '.' : screen.textContent = screen.textContent;
-            break;
-        case 'plusMinus':
-            screen.textContent = -parseFloat(screen.textContent) ///////stooppppeeddd heereee
-            console.log(numberOne, numberTwo);
-            if (!!numberOne) numberTwo = screen.textContent;
-            break;
-        default: 
-            executeCalculation(); 
     }
 }
 
-const numericalSelected = function(number) {
-    screen.textContent === '0' ? screen.textContent = number : screen.textContent += number;
-    if (!!numberOne) {
-        if (!!prevAction) { screen.textContent = number; prevAction = null; }
-        numberTwo = screen.textContent;
-        console.log('no2: '+numberTwo);
+const functionCall = function(operation) {
+    if (operation.classList.contains('math')) {
+        //wszystkie operacje dod,odej,mnoz, dzielenia
+        if (!cal.operandOne) {
+            //jesli nie ma pierwszego czlona
+            cal.operation = operation.innerText;
+            cal.operandOne = screen.innerText;
+            cal.displayToBeErased = true;
+            console.table(cal);
+        }
+        else if (!!cal.operandOne && !!cal.operation && cal.operandTwoPending === true) {
+            cal.operation = operation.innerText;
+            console.table(cal);
+        }
+        else if (!!cal.operandOne && !!cal.operation && cal.operandTwoPending === false) {
+            cal.operandTwo = screen.innerText;
+            cal.tempoperand = cal.operandOne; //moze sie przyda
+            console.table(cal);
+            screen.innerText = calculate(); //OSTATNIA ZMIANA
+            cal.displayToBeErased = true;
+            cal.operandTwoPending = true;
+            cal.operandOne = screen.innerText;
+        }
+        cal.operation = operation.innerText;
+        
+    }
+    if (operation.innerText === '=' && !!cal.operandOne) { //MAMY pierwszy operator i operation
+        if (cal.operandTwoPending === false) {
+        cal.operandTwo = screen.innerText;
+        cal.tempoperand = cal.operandOne;
+        screen.innerText = calculate();
+        cal.displayToBeErased = true;
+        cal.operandTwoPending = true;
+        cal.operandOne = screen.innerText;
+        return;
+        }
+        if (cal.operandTwoPending === true) {
+            screen.innerText = calculate();
+            cal.operandOne = screen.innerText;
+
+        }
+    }
+    if (operation.innerText === 'C') { 
+        cal.operandOne = null;
+        cal.operandOne = null;
+        cal.operandTwo = null;
+        cal.operation = null;
+        cal.operandTwoPending = true;
+        cal.tempoperand = null;
+        cal.displayToBeErased = false;
+        screen.innerText = '0';
+    }
+    if (operation.innerText === '.') { 
+        if (screen.innerText === '0') {
+            screen.innerText = '0.';
+        }
+        if (!screen.innerText.includes('.')) {
+            screen.innerText += '.';
+        }
+        if (!!cal.operandOne && cal.operandTwoPending === true) {
+            screen.innerText = '0.'
+            cal.displayToBeErased = false;
+        }
+    }
+    if (operation.innerText === '+/-') { 
+        screen.innerText = -screen.innerText;
+        if (!!cal.operandOne) cal.operandOne = screen.innerText;
     }
 }
 
-const executeCalculation = function() {
-    switch (operator) {
-        case 'add':
-            numberOne = parseFloat(numberOne) + parseFloat(numberTwo);
-            numberTwo = null;
-            operator = null;
-            prevAction = 'add';
-            break;
-        case 'divide':
-            if (numberTwo === '0') { 
-                numberOne = 'WTF?'; 
-                numberTwo = null;
-                operator = null;
-                prevAction = 'divide';
-                break;
-            }
-            numberOne = parseFloat(numberOne) / parseFloat(numberTwo);
-            numberTwo = null;
-            operator = null;
-            prevAction = 'divide';
-            break;
-        case 'multiply':
-            numberOne = parseFloat(numberOne) * parseFloat(numberTwo);
-            numberTwo = null;
-            operator = null;
-            prevAction = 'multiply';
-            break;
-        case 'substract':
-            numberOne = parseFloat(numberOne) - parseFloat(numberTwo);
-            numberTwo = null;
-            operator = null;
-            prevAction = 'substract';
-            break;
+const calculate = function(operation) {
+    let result = null;
+    if (cal.operation === '+') result = parseFloat(cal.operandOne) + parseFloat(cal.operandTwo);
+    if (cal.operation === '-') result = parseFloat(cal.operandOne) - parseFloat(cal.operandTwo);
+    if (cal.operation === '/') result = parseFloat(cal.operandOne) / parseFloat(cal.operandTwo);
+    if (cal.operation === 'x') result = parseFloat(cal.operandOne) * parseFloat(cal.operandTwo);
+    result = result.toString();
+    if (result.length < 9) return result;
+    else {
+        result = parseFloat(result).toExponential(2);
+        return result;
     }
-    screen.textContent = new Intl.NumberFormat("en-US", {  //allowing for max 6 decimals
-        style: "decimal",
-        maximumFractionDigits: 6,
-    }).format(numberOne);
+
 }
+
+/* add keyboard support
+change names of operand to oPerand
+error message when dividing by zero
+highlight the operation that is being selected
+great design (full screen?)
+DONE changing the sign (+/-) of a result should save it (UPDATE) as a new operandOne
+history of inputs  */
